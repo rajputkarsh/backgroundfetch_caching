7d84abeaee42552a2e7edd0e51b4f7639eddaf2b
Utkarsh <r.utkarsh.0010@gmail.com>
2023-04-15 23:53:10 +0530

    add a simple service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { register } from './serviceWorker';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+// register service worker
+register({});
